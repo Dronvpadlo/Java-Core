29d28f62699bdfff6c4dbb93466779e4256b89d7
User <doronyuk714@mail.com>
2024-09-07 13:12:03 +0300

    Complete HW

--- src/HW1/Main.java
+++ src/HW1/Main.java
@@ -51,5 +51,14 @@ public class Main {
         posts.add(post4);
         posts.add(post5);
         System.out.println(posts);
+
+        //Comments
+        Comment[] comments = new Comment[5];
+        comments[0] = new Comment(1,1,"id labore ex et quam laborum", "Eliseo@gardner.biz", "laudantium enim quasi est quidem magnam voluptate ipsam eos\\ntempora quo necessitatibus\\ndolor quam autem quasi\\nreiciendis et nam sapiente accusantium");
+        comments[1] = new Comment(1,4, "alias odio sit", "Lew@alysha.tv", "non et atque\\noccaecati deserunt quas accusantium unde odit nobis qui voluptatem\\nquia voluptas consequuntur itaque dolor\\net qui rerum deleniti ut occaecati");
+        comments[2] = new Comment(2,7, "repellat consequatur praesentium vel minus molestias voluptatum", "Dallas@ole.me", "maiores sed dolores similique labore et inventore et\\nquasi temporibus esse sunt id et\\neos voluptatem aliquam\\naliquid ratione corporis molestiae mollitia quia et magnam dolor");
+        comments[3] = new Comment(3,11, "fugit labore quia mollitia quas deserunt nostrum sunt", "Veronica_Goodwin@timmothy.net", "ut dolorum nostrum id quia aut est\\nfuga est inventore vel eligendi explicabo quis consectetur\\naut occaecati repellat id natus quo est\\nut blanditiis quia ut vel ut maiores ea");
+        comments[4] = new Comment(6,27,"doloribus quibusdam molestiae amet illum", "doloribus quibusdam molestiae amet illum", "nisi vel quas ut laborum ratione\\nrerum magni eum\\nunde et voluptatem saepe\\nvoluptas corporis modi amet ipsam eos saepe porro");
+        System.out.println(Arrays.toString(comments));
         }
     }
