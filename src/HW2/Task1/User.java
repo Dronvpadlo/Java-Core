43ea2cc3339e2b7c55135a8aa4a02f54f7a916d6
User <doronyuk714@mail.com>
2024-09-10 21:35:58 +0300

    1 task complete

--- src/HW2/Task1/User.java
+++ src/HW2/Task1/User.java
@@ -41,58 +41,6 @@ public class User {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getWebsite() {
-        return website;
-    }
-
-    public void setWebsite(String website) {
-        this.website = website;
-    }
-
-    public Company getCompany() {
-        return company;
-    }
-
-    public void setCompany(Company company) {
-        this.company = company;
-    }
-
     public String toString(){
         return name + " {ID: " + id + ", Name: " + name + ", Username: " + username + ", Email: " + email + address + ", Phone: " + phone + ", Website: " + website + company + "}";
     }
