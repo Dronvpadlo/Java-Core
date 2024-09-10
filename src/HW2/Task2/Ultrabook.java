13305fd3daa390ff395a3e1022eab2f60c5d2cce
User <doronyuk714@mail.com>
2024-09-10 21:36:07 +0300

    2 task complete

--- src/HW2/Task2/Ultrabook.java
+++ src/HW2/Task2/Ultrabook.java
@@ -1,6 +1,5 @@
 package HW2.Task2;
 
-import HW2.Task2.Laptop;
 
 public class Ultrabook extends Laptop {
     private double weight;
@@ -11,16 +10,6 @@ public class Ultrabook extends Laptop {
     public Ultrabook(String pcName, String processorName, double frequency, int ram, double displaySize, boolean keyboardLighting, double weight) {
         super(pcName, processorName, frequency, ram, displaySize, keyboardLighting);
         this.weight = weight;
-    }
-
-    public double getWeight() {
-        return weight;
-    }
-
-    public void setWeight(double weight) {
-        if (weight>0) {
-            this.weight = weight;
-        }
     }
 
     @Override
