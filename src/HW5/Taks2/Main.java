89558a49d56978de4b00aa6eb22c1b933b53a2ff
User <doronyuk714@mail.com>
2024-09-19 17:14:51 +0300

    Complete Task2

--- src/HW5/Taks2/Main.java
+++ src/HW5/Taks2/Main.java
@@ -1,12 +1,43 @@
 package HW5.Taks2;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
         //- Створити масив з 20 числами.
-        //- за допомогою способу sorted відсортувати масив.
-        //-- за допомогою filter отримати числа кратні 3
-        //-- за допомогою filter отримати числа кратні 10
-        //-- перебрати (проітерувати) масив за допомогою foreach()
-        //-- перебрати масив за допомогою map() і отримати новий масив, в якому всі значення будуть у 3 рази більші
+        ArrayList<Integer> integers = new ArrayList<>();
+        integers.add(10);
+        integers.add(7);
+        integers.add(12);
+        integers.add(5);
+        integers.add(90);
+        integers.add(9);
+        integers.add(15);
+        integers.add(4);
+        integers.add(17);
+        integers.add(30);
+        integers.add(19);
+        integers.add(69);
+        integers.add(2);
+        integers.add(3);
+        integers.add(11);
+        integers.add(6);
+        integers.add(14);
+        integers.add(8);
+        integers.add(16);
+        integers.add(0);
+        Stream<Integer> stream = integers.stream();
+            stream
+                .sorted()  //- за допомогою способу sorted відсортувати масив.
+                .filter(integer -> integer % 3 == 0) //-- за допомогою filter отримати числа кратні 3
+                .filter(integer -> integer % 10 == 0) //-- за допомогою filter отримати числа кратні 10
+                .map(integer -> integer * 3)  //-- перебрати масив за допомогою map() і отримати новий масив, в якому всі значення будуть у 3 рази більші
+                .collect(Collectors.toList())
+                .forEach(integer -> System.out.println(integer)); //-- перебрати (проітерувати) масив за допомогою foreach()
+
+
     }
 }
