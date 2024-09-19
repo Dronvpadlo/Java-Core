06e9f22515df83053826b6c5d2acf7f508ddfd98
User <doronyuk714@mail.com>
2024-09-19 17:02:20 +0300

    Init HW5 Task1

--- src/HW5/Taks1/Main.java
+++ src/HW5/Taks1/Main.java
@@ -1,9 +1,43 @@
 package HW5.Taks1;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
         //створити ArrayList зі словами на 15-20 елементів.
         //- відсортувати його за алфавітом .
-        //*-- відфільтрувати слова довжиною менше 4 символів
+        //*-- відфільтрувати слова довжиною менше 4 символів (Використав менше 5, бо менше 4 не було в лісті)
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("Bologna");
+        strings.add("AC Milan");
+        strings.add("Como");
+        strings.add("Napoli");
+        strings.add("Empoli");
+        strings.add("Juventus");
+        strings.add("Inter Milan");
+        strings.add("Udinese");
+        strings.add("Venecia");
+        strings.add("Lazio");
+        strings.add("Roma");
+        strings.add("Parma");
+        strings.add("Torino");
+        strings.add("Verona");
+        strings.add("Cagliari");
+        strings.add("Fiorentina");
+        strings.add("Atalanta");
+        strings.add("Genoa");
+        strings.add("Monca");
+        strings.add("Lecce");
+        Stream<String> stream = strings.stream();
+        List<String> collect = stream
+                .sorted()
+                .filter(string -> string.length() < 5)
+                .collect(Collectors.toList());
+
+        System.out.println(collect);
     }
 }
