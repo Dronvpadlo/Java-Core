c2643b3262a46138785e512c98e965428479478f
User <doronyuk714@mail.com>
2024-09-19 16:28:03 +0300

    Init HW5

---
+++ src/HW5/Taks1/Main.java
@@ -0,0 +1,9 @@
+package HW5.Taks1;
+
+public class Main {
+    public static void main(String[] args) {
+        //створити ArrayList зі словами на 15-20 елементів.
+        //- відсортувати його за алфавітом .
+        //*-- відфільтрувати слова довжиною менше 4 символів
+    }
+}
