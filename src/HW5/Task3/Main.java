e39bc44830e134ce3f438d39e2ca732ca836a86f
User <doronyuk714@mail.com>
2024-09-19 20:50:08 +0300

    Complete HW5

--- src/HW5/Task3/Main.java
+++ src/HW5/Task3/Main.java
@@ -2,10 +2,7 @@ package HW5.Task3;
 
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
